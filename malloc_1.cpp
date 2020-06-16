419e5825424fd3fbbeb9f9aed02b2cd98be577a5
Elad Cohen <eladc94@gmail.com>
2020-06-16 13:55:14 +0000

    .

--- malloc_1.cpp
+++ malloc_1.cpp
@@ -1,5 +1,10 @@
 #include <unistd.h>
 
 void* smalloc(size_t size){
-
+    if (0 == size || size > 100000000)
+        return NULL;
+    void* address = sbrk(size);
+    if ((long)address == -1)
+        return NULL;
+    return address;
 }
